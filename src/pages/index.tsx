bb95f261855205d271660508a81cbc5bd3c3eefc
Gustavo Galo <gg.gustavo.galo@gmail.com>
2022-02-06 12:26:45 -0300

    feat: index page.

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import { Container, Loader } from "./styles";
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      <Loader />
+      <div>In progress...</div>
+    </Container>
+  );
+};
+
+export default Home;
